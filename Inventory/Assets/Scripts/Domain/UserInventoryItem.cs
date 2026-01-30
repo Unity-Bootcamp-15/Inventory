e796c395a396c68351ebd20c35b4aec80b084057
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2026-01-30 09:30:48 +0900

    refactor: UserInventoryItem에 SerialNumber 생성 로직 추가

--- Inventory/Assets/Scripts/Domain/UserInventoryItem.cs
+++ Inventory/Assets/Scripts/Domain/UserInventoryItem.cs
@@ -1,11 +1,21 @@
+using System;
+
 public sealed class UserInventoryItem : Entity<long>
 {
     public long SerialNumber => Id;
     public int ItemId { get; }
 
-    public UserInventoryItem(long serialNumber, int itemId)
+    private UserInventoryItem(long serialNumber, int itemId)
         : base(serialNumber)
     {
         ItemId = itemId;
+    }
+
+    public static UserInventoryItem Create(int itemId)
+    {
+        Random randomGenerator = new Random();
+        long serialNumber = long.Parse(DateTime.Now.ToString("yyyyMMddHHmmss") + randomGenerator.Next(9999).ToString("D4"));
+        
+        return new UserInventoryItem(serialNumber, itemId);
     }
 }
