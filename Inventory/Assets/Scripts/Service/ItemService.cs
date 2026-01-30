5e8d32c439139e9ad664f244e541117c38041adb
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2026-01-30 10:50:46 +0900

    feat: ItemService 추가

---
+++ Inventory/Assets/Scripts/Service/ItemService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public sealed class ItemService
+{
+    private readonly IItemRepository _itemRepository;
+
+    public ItemService(IItemRepository itemRepository)
+    {
+        Debug.Assert(itemRepository != null);
+
+        _itemRepository = itemRepository;
+    }
+
+    public int GetRandomId()
+    {
+        // 1. 모든 아이템 목록을 가져온다.
+        IReadOnlyList<Item> items = _itemRepository.All;
+        // 2. 무작위로 아이템을 하나 선정한다.
+        Random randomGenerator = new Random();
+        int randomIndex = randomGenerator.Next(items.Count);
+        Item randomItem = items[randomIndex];
+        // 3. 아이템의 ID를 반환한다.
+        return randomItem.Id;
+    }
+}
