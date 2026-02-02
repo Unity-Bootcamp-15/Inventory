b834c221b280ffe187b3ad3f31c4e0552008557a
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2026-02-02 14:29:46 +0900

    feat(InventoryUI): InventoryItemSlotData에 데이터 설정 구현

--- Inventory/Assets/Scripts/UI/InventoryUI.cs
+++ Inventory/Assets/Scripts/UI/InventoryUI.cs
@@ -5,6 +5,7 @@ public class InventoryUI : MonoBehaviour
 {
     [SerializeField] private InfiniteScroll _scroll;
     [SerializeField] private UserInventoryServiceLocatorSO _userInventoryServiceLocator;
+    [SerializeField] private ItemServiceLocatorSO _itemServiceLocator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,10 +20,12 @@ public class InventoryUI : MonoBehaviour
         // 인벤토리에 있는 아이템을 가져와서 => UserInventoryService
         foreach (BagItem item in _userInventoryServiceLocator.Service.UnequippedItems)
         {
-            // item.ItemId에 해당되는 Grade 스프라이트, Icon 스프라이트
-            
-            // _scroll에 추가한다.
             var data = new InventoryItemSlotData();
+            string gradeSpritePath = _itemServiceLocator.Service.GetGradeSpritePath(item.ItemId);
+            data.ItemGradeSprite = Resources.Load<Sprite>(gradeSpritePath);
+            string iconSpritePath = _itemServiceLocator.Service.GetIconSpritePath(item.ItemId);
+            data.ItemIconSprite = Resources.Load<Sprite>(iconSpritePath);
+
             _scroll.InsertData(data);
         }
     }
